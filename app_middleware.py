3cbfd0f3c047bca197d3abe929dacf4a7ceeb9f4
nie3e <adrcwiek@gmail.com>
2023-05-27 16:15:36 +0200

    Adds support for manual and docker run

--- app_middleware.py
+++ app_middleware.py
@@ -1,13 +1,17 @@
 from werkzeug.middleware.http_proxy import ProxyMiddleware
 from werkzeug.serving import run_simple
 from flask import Flask
+import os
+
+api_v1 = "api_v1" if os.getenv("PLATFORM", None) == "DOCKER" else "localhost"
+api_v2 = "api_v2" if os.getenv("PLATFORM", None) == "DOCKER" else "localhost"
 
 app = ProxyMiddleware(Flask(__name__), {
     "/v1/": {
-        "target": "http://api_v1:5000/v1/"
+        "target": f"http://{api_v1}:5000/v1/"
     },
     "/v2/": {
-        "target": "http://api_v2:5001/v2/"
+        "target": f"http://{api_v2}:5001/v2/"
     }
 })
 
